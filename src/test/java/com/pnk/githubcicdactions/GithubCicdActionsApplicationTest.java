ea1449b034a5aad9569e207ff16328ee7ec7163d
Phong Vo <vhphong@gmail.com>
2024-01-10 11:27:36 -0600

    added testWelcome.. changed java ver to 21 in pom.xml and maven.yml

---
+++ src/test/java/com/pnk/githubcicdactions/GithubCicdActionsApplicationTest.java
@@ -0,0 +1,21 @@
+package com.pnk.githubcicdactions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GithubCicdActionsApplicationTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+
+    @Test
+    public void testWelcome() {
+    }
+}
